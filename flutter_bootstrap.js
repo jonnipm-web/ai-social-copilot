d7551b6775c6551c2bff1136a8238603593ff6f2
jonnipm-web <jonnipm-web@users.noreply.github.com>
2026-06-27 20:51:34 +0000

    deploy: 8e67608877e211064794a95a19fbdaada5b7f81f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2571846110"
+    serviceWorkerVersion: "2449548582"
   }
 });
